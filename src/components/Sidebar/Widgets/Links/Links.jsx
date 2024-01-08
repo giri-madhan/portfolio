796f4f46b12e2b3e3bc3c261930e13f1037fd4fc
Giri Madhan <girimadhan7@gmail.com>
2024-01-08 06:26:03 +0530

    feat: side bar animation and links

---
+++ src/components/Sidebar/Widgets/Links/Links.jsx
@@ -0,0 +1,42 @@
+import { motion } from "framer-motion";
+
+const variants = {
+  open: {
+    transition: {
+      staggerChildren: 0.1,
+    },
+  },
+  closed: {
+    transition: {
+      staggerChildren: 0.05,
+      staggerDirection: -1,
+    },
+  },
+};
+
+const itemVariants = {
+  open: {
+    y: 0,
+    opacity: 1,
+  },
+  closed: {
+    y: 50,
+    opacity: 0,
+  },
+};
+
+const Links = () => {
+  const links = ["Homepage", "Services", "Portfolio", "Contact", "About"];
+
+  return (
+    <motion.div className="links" variants={variants}>
+      {links.map((link, index) => (
+        <motion.a key={link + index} href={`#${link}`} variants={itemVariants}>
+          {link}
+        </motion.a>
+      ))}
+    </motion.div>
+  );
+};
+
+export default Links;
