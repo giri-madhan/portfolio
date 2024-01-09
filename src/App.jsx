a8eb882313c25c5bd21cea23374b01b9d78260e8
Giri Madhan <girimadhan7@gmail.com>
2024-01-10 02:04:55 +0530

    feat: parallex effect

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,22 @@
 import "./app.scss";
+import Hero from "./components/Hero/Hero";
 import Navbar from "./components/Navbar/Navbar";
+import Parallax from "./components/Parallax/Parallax";
 
 const App = () => {
   return (
     <div>
       <section id="Homepage">
         <Navbar />
+        <Hero />
       </section>
-      <section id="Services">Parallax</section>
+      <section id="Services">
+        <Parallax type="services" />
+      </section>
       <section>Services</section>
-      <section id="Portfolio">Parallax</section>
+      <section id="Portfolio">
+        <Parallax type="portfolio" />
+      </section>
       <section>Portfolio1</section>
       <section>Portfolio2</section>
       <section>Portfolio3</section>
